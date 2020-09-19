35393d5dde5b255c2b3ed02012416048ee528b61
amalanchuk <artem.malanchuk@gmail.com>
2020-09-19 13:51:27 +0300

    finish

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+const ADMIN_PASSWORD = "jqueryismyjam";
+let message;
+
+let input = prompt("Введите пароль!");
+
+if (input === null) {
+    message = "Отменено пользователем!";
+} else if (input === ADMIN_PASSWORD) {
+    message = "Добро пожаловать!";
+} else {
+    message = "Доступ запрещен, неверный пароль!";
+}
+
+alert(message);
